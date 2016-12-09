2896de6685c2a5fce6e749a5f7af024d8c5912b0
zhongl <zhong.lunfu@gmail.com>
2016-12-09 16:52:24 +0800

    Tag basic.

--- agent/src/main/java/me/zhongl/agent/Main.java
+++ agent/src/main/java/me/zhongl/agent/Main.java
@@ -1,6 +1,11 @@
 package me.zhongl.agent;
 
+import net.bytebuddy.agent.ByteBuddyAgent;
+
+import java.io.File;
 import java.lang.instrument.Instrumentation;
+import java.net.URISyntaxException;
+import java.net.URL;
 
 public class Main {
     public static void premain(String args, Instrumentation inst) {
@@ -10,4 +15,19 @@ public class Main {
     public static void agentmain(String args, Instrumentation inst) {
         System.out.println("This is a agentmain callback");
     }
+
+    public static void main(String[] args) {
+        try {
+            ByteBuddyAgent.attach(agentJar(), args[0]);
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.err.println("Usage : java -jar agent.jar <PID>");
+        }
+    }
+
+    private static File agentJar() throws URISyntaxException {
+        final URL url = Main.class.getProtectionDomain().getCodeSource().getLocation();
+        return new File(url.toURI());
+    }
+
 }
--- demo/src/main/java/me/zhongl/demo/Main.java
+++ demo/src/main/java/me/zhongl/demo/Main.java
@@ -2,6 +2,7 @@ package me.zhongl.demo;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("This is a demo application");
+        System.out.println("This is a demo application, press enter to quit.");
+        System.console().readLine();
     }
 }
