a085441e145e6cb1f74be006e70aa5de1cbac344
zhongl <zhong.lunfu@gmail.com>
2016-12-11 21:29:01 +0800

    Wrapped transformed class method.

--- agent/src/main/java/me/zhongl/agent/Main.java
+++ agent/src/main/java/me/zhongl/agent/Main.java
@@ -1,19 +1,25 @@
 package me.zhongl.agent;
 
 import net.bytebuddy.agent.ByteBuddyAgent;
+import net.bytebuddy.agent.builder.AgentBuilder;
+import net.bytebuddy.implementation.MethodDelegation;
 
 import java.io.File;
 import java.lang.instrument.Instrumentation;
 import java.net.URISyntaxException;
 import java.net.URL;
 
+import static net.bytebuddy.matcher.ElementMatchers.named;
+
 public class Main {
     public static void premain(String args, Instrumentation inst) {
         System.out.println("This is a premain callback");
+        main(args, inst);
     }
 
     public static void agentmain(String args, Instrumentation inst) {
         System.out.println("This is a agentmain callback");
+        main(args, inst);
     }
 
     public static void main(String[] args) {
@@ -28,6 +34,15 @@ public class Main {
     private static File agentJar() throws URISyntaxException {
         final URL url = Main.class.getProtectionDomain().getCodeSource().getLocation();
         return new File(url.toURI());
+    }
+
+    private static void main(String args, Instrumentation inst) {
+        new AgentBuilder.Default()
+                .type(named("me.zhongl.demo.Main"))
+                .transform((builder, typeDescription, classloader) -> {
+                    return builder.method(named("main"))
+                                  .intercept(MethodDelegation.to(TimingInterceptor.class));
+                }).installOn(inst);
     }
 
 }
